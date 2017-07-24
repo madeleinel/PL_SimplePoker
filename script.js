5f1797522e4a08cc0c2d790dd7ae33fd3193650c
madeleinel <madeleinelinder@hotmail.com>
2017-07-24 17:16:14 +0100

    Shuffling the deck of cards (within the deck() function, before returning it)

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Create the card deck
-
-// Create a deck object which returns an array of 52 cards (ie the full deck)
+//////// Create the card deck ////////
+// Create a deck object which shuffles and returns an array of 52 cards (ie the full deck)
 function deck() {
   this.names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]; // Specify the possible card names
   this.suits = ["Spades", "Diamonds", "Clubs", "Hearts"]; // Specify the possible card suits
@@ -20,8 +19,29 @@ function deck() {
       cards.push(new card(this.suits[s], this.names[n], n+1)); // Push each card to the "cards" array
     }
   }
+
+  // Now that the deck has been created;
+  // Use the Fisher-Yates (aka Knuth) Shuffle function found in the top response on https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  // Shuffle the card deck using the function above
+  cards = shuffle(cards);
   console.log(cards);
-  return cards; // Return the "cards" array to make it available outside of the function
+  // Return the "cards" array to make it available outside of the function
+  return cards;
 }
 
 // Create a card object which accepts the name, suit and value of each card
@@ -32,4 +52,6 @@ function card(suit, name, value) { // Note that the arguments of this function n
   this.suit = suit;
 }
 
+// Run the deck() function
+// >> change this into an IIFE || make it run on window.onLoad() <<
 deck();
